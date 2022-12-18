8ab8f60e2eb492b68d38340da571b82fd7f18950
Daniel-Machuca <Daniel.Machuca@alu.uclm.es>
2022-12-18 10:44:04 +0100

    Corregida la funcionalidad isLeapYear y quitada de la clase DateController para ser añadida en la clase Date  #4

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -36,5 +36,25 @@ public class Date {
 	public String toString() {
 		return "Date: " + day + "/" + month + "/" + year;
 	}
+	
+	public boolean isLeapYear(Date t) {
+		// if para comprobar que los valores no sean negativos o 0
+		if (t.getDay() <= 0 || t.getMonth() <= 0 || t.getYear() < 0) {
+			throw new NumerosValidosException("Numeros negativos o 0 no validos");
+		}
+		if (t.getYear() % 4 == 0) {
+			if (t.getYear() % 100 == 0) {
+				if (t.getYear() % 400 == 0) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
 
 }
